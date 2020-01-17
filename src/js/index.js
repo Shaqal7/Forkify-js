5c03c22ae38e4ed69ac26698b5015c832f158253
Shaqal7 <shaqal7@gmail.com>
2020-01-17 17:01:57 +0100

    making our First API Calls cd

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,2 @@
 // Global app controller
-import num from './test';
-
-const x = 23;
-
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import Search from "../../../final/src/js/models/Search";
---
+++ src/js/model/Search.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+async function getResults(query) {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+    //console.log(res);
+}
+//const res = await Axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+
+getResults('pizza').then(res => console.log(res));
