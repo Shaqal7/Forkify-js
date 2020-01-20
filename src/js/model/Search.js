167ba71e9885b5f7017c8362796258cd3ba18491
Tomasz Śmierzchała <tsmierzchala@forcom.com.pl>
2020-01-20 10:51:34 +0100

    add try catch block for Search.js

--- src/js/model/Search.js
+++ src/js/model/Search.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 async function getResults(query) {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-    console.log(res);
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (e) {
+        alert(e);
+    }
+
 }
 //const res = await Axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
 
