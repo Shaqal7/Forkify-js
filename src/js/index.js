c40a4decc6e2c52a8e4cd229e733e03b8b404452
Tomasz Śmierzchała <tsmierzchala@forcom.com.pl>
2020-01-20 10:57:45 +0100

    rename model as models and some cleanup in index.js and Search.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // Global app controller
-import Search from "./model/Search";
+import Search from "./models/Search";
+
+import axios from "axios";
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (e) {
+        alert(e);
+    }
+
+}
+//const res = await Axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+
+getResults('pizza');
