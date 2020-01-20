cfe8d02aa7f9ae160c51014c719a5f420c2d56fe
Tomasz Śmierzchała <tsmierzchala@forcom.com.pl>
2020-01-20 11:06:44 +0100

    Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,7 @@
 // Global app controller
 import Search from "./models/Search";
 
-import axios from "axios";
+const search = new Search('pizza');
+console.log(search);
 
-async function getResults(query) {
-    try {
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (e) {
-        alert(e);
-    }
-
-}
-//const res = await Axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-
-getResults('pizza');
+search.getResults();
