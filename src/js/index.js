11d41cc4f565d70504842c0835c3ef849249653f
Shaqal7 <shaqal7@gmail.com>
2020-01-20 16:28:15 +0100

    Implementing Search Results Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -37,3 +37,11 @@ elements.searchForm.addEventListener('submit', evt => {
    controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+   if (btn) {
+       const goToPage = parseInt(btn.dataset.goto, 10);
+       searchView.clearResults();
+       searchView.renderResults(state.search.result, goToPage);
+   }
+});
