6f091003dbec2573b5b40be1bec1da3ce446b808
Shaqal7 <shaqal7@gmail.com>
2020-01-21 14:48:17 +0100

    Building the Recipe Model - Part 1

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,1 +1,35 @@
-//const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+import axios from "axios";
+//import { url } from '../config';
+
+export default class Recipe {
+    constructor(id) {
+        this.id = id;
+    }
+
+    async getRecipe() {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.result = res.data.recipe;
+            this.title = this.result.title;
+            this.author = this.result.publisher;
+            this.img = this.result.image_url;
+            this.url = this.result.source_url;
+            this.ingredients = this.result.ingredients;
+            this.rank = this.result.social_rank;
+        } catch (error) {
+            console.log(error);
+            alert('Something went wrong :(');
+        }
+    }
+
+    calcTime() {
+        // Assuming that we need 15 min for each 3 ingredients
+        const numIng = this.ingredients.length;
+        const periods = Math.ceil(numIng / 3);
+        this.time = periods * 15;
+    }
+
+    calcServings() {
+        this.servings = 4;
+    }
+}
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
+//import { url } from '../config';
 
 export default class Search {
     constructor(query) {
